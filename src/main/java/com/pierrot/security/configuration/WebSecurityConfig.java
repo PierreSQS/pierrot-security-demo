fbfb3cb58c65a3f14aa038d15918fb8c6c0feecc
PierreSQS <nomo_messina@web.de>
2022-01-31 22:43:50 +0100

    Commit03-NOK: added Security like habuma but doesn't work

--- src/main/java/com/pierrot/security/configuration/WebSecurityConfig.java
+++ src/main/java/com/pierrot/security/configuration/WebSecurityConfig.java
@@ -1,10 +1,25 @@
 package com.pierrot.security.configuration;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("Pierrot")
+                    .password("Pierrot123")
+                    .authorities("ROLE_USER")
+                .and()
+                .withUser("Odile")
+                    .password("Odile123")
+                    .authorities("ROLE_USER");
+
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // Not implemented yet
     }
 }
