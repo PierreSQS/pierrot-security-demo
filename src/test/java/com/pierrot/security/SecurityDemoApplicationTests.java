f2bc34db7fe3fa73234c793af858f32b6ac4fd7b
PierreSQS <nomo_messina@web.de>
2022-01-31 21:33:42 +0100

    Commit02: renamed classes

---
+++ src/test/java/com/pierrot/security/SecurityDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.pierrot.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class SecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads(ApplicationContext appCtx) {
+		assertThat(appCtx).isNotNull();
+	}
+
+}
