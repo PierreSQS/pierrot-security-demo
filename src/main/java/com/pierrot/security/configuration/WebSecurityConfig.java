974d5d0fafb79140e057e0cf2cc9df92f5e23877
PierreSQS <nomo_messina@web.de>
2022-03-05 15:16:09 +0100

    Commit04: fixed the issue of the previous commit(fbfb3cb5)

--- src/main/java/com/pierrot/security/configuration/WebSecurityConfig.java
+++ src/main/java/com/pierrot/security/configuration/WebSecurityConfig.java
@@ -1,25 +1,38 @@
 package com.pierrot.security.configuration;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("Pierrot")
-                    .password("Pierrot123")
+                    .password("{bcrypt}$2a$10$u5K1acOFC7HZrrPjTwQVv.3zQWkUrk7afB21UQeipOzZJ/9w74lR.")
                     .authorities("ROLE_USER")
                 .and()
                 .withUser("Odile")
-                    .password("Odile123")
+                    .password("{bcrypt}$2a$10$oxvtnswHxzkwTF3ts/k0futaOdwQXF7yGN.8vLiveXdCjdDO93bc6")
                     .authorities("ROLE_USER");
 
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // Not implemented yet
+        http
+                .authorizeRequests()
+                    .antMatchers("/login")
+                    .permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                    .formLogin()
+                    .permitAll()
+                .and()
+                .logout().permitAll();
     }
 }
